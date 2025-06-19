78e1753e9c8d57cd58885336f982ea5eba157857
fabro00150 <richard.farinango0590@utc.edu.ec>
2025-06-19 08:18:03 -0500

    writable 6

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -5,6 +5,10 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        try{
+            return view('home/index');
+        }catch(\Exception $ex){
+            die('Error: ' . $ex->getMessage());
+        }
     }
 }
